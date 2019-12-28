86f3e979010586e42d6f3affd33c1cd23180b351
Basith Kunimal <basith@intellicar.in>
2019-12-28 14:45:10 +0530

    map fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default () => {
     loadJS();
     setTimeout(() => {
       document.getElementById('map').innerHTML = '';
-      let map = window.L.map('map', {scrollWheelZoom: false}).setView([12.104083, 75.203500], 14);
+      let map = window.L.map('map', {scrollWheelZoom: false, dragging: false}).setView([12.104083, 75.203500], 14);
       window.L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
