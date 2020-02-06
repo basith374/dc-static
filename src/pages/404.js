de61934b8a831bbe41a63fff82ff15d883062720
Basith Kunimal <basith@intellicar.in>
2020-02-06 17:46:04 +0530

    added header to 404 & areas served

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Head } from 'react-static';
+import Header from '../containers/Header';
 
 export default () => (
-  <div className="About">
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
+  <div>
+    <Head>
+      <title>Page not found</title>
+    </Head>
+    <Header />
+    <div className="About">
+      <h1>404 - We couldn't find that page :(</h1>
+    </div>
   </div>
 )
--- src/pages/areas-served.js
+++ src/pages/areas-served.js
@@ -2,20 +2,24 @@ import React from 'react'
 import { Head } from 'react-static';
 import { getPlaces } from '../common';
 import { Link } from 'components/Router'
+import Header from '../containers/Header';
 
 export default () => (
-  <div className="About">
+  <div>
     <Head>
         <title>Areas served</title>
         <meta name="keywords" content="kalyana,mandapa,catering,food,biriyani,south,indian,decoration,lights,music,stage,balloon,panthal,tent,bridal,makeover" />
         <meta name="description" content="We are a professional service company here to help you plan &amp; undertake your public/private events." />
     </Head>
-    <h1>Areas served</h1>
-    <div className="plinks">
-        {getPlaces().map((p, i) => {
-            let slug = '/find/event-managers-in-' + p.name.toLowerCase().replace(/\s/, '-');
-            return <Link to={slug} key={i}>{p.name}</Link>
-        })}
+    <Header />
+    <div className="About">
+      <h1>Areas served</h1>
+      <div className="plinks">
+          {getPlaces().map((p, i) => {
+              let slug = '/find/event-managers-in-' + p.name.toLowerCase().replace(/\s/, '-');
+              return <Link to={slug} key={i}>{p.name}</Link>
+          })}
+      </div>
     </div>
   </div>
 )
